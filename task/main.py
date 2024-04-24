b36bb8698c721569c22601f17816f74bed7df4ff
wRiez <157474857+wRiez@users.noreply.github.com>
2024-04-24 12:23:42 +0300

    Add files via upload

---
+++ task/main.py
@@ -0,0 +1,71 @@
+import requests
+
+def get_students():
+    i = 0
+    response = requests.get('http://127.0.0.1/students')
+    all = response.json()
+    print('Все студенты: ')
+    while i < len(all):
+        print(f'{i+1}-й: {all[i]} \n')
+        i += 1
+    return all
+
+def get_one_student(Num):
+    url = 'http://127.0.0.1/students/'
+    if (int(Num) > 3) or (int(Num) < 0):
+        print("Некорректный номер студента.")
+    else:
+        url = url + Num
+        # print(url)
+        response = requests.get(url)
+        one = response.json()
+        print(f'Студент номер {Num}: \n\t{one} \n')
+
+def new_student():
+    data_ = params()
+    response = requests.post('http://127.0.0.1/students', json=data_)
+    if response:
+        return ({"message": "Студент успешно добавлен", "student": data_}), 201
+    # get_one_student(id_)
+
+def update_students():
+    print('\nОбновление информации: ')
+    id_ = id_make()
+    data_ = params()
+    url = 'http://127.0.0.1/students/' + str(id_)
+    response = requests.put(url, json=data_)
+    if response:
+        return ({"message": "Данные студента успешно обновлены", "student": data_}), 201
+
+def delete_stud():
+    id_ = id_make()
+    url = 'http://127.0.0.1/students/' + str(id_)
+    response = requests.delete(url)
+    if response:
+        return ({'message': 'Студент успешно удален'}), 201
+
+
+# Переменные #
+
+Num = str(input('Номер студента: \n'))
+
+def id_make():
+    print('\n')
+    id_ = int(input('ID студента: '))
+    return id_
+def params():
+    id_ = int(input('Номер нового студента: '))
+    name = str(input('Имя нового студента: '))
+    age = int(input('Возраст нового студента: '))
+    major = str(input('Специальность нового студента: '))
+    data_ = {'id': id_, 'name': name, 'age': age, 'major': major}
+    return data_
+
+
+# Выполнение #
+
+get_students()
+get_one_student(Num)
+new_student()
+update_students()
+delete_stud()
